1b33cf1d1aca4b4071f3582d7b7b5bfa1191c3ac
Joseph Scheuhammer <clown@alum.mit.edu>
2021-03-17 14:26:08 -0400

    fix: environment variables for user and database names

--- src/database/scripts/dropDockerTestDatabase.sh
+++ src/database/scripts/dropDockerTestDatabase.sh
@@ -12,8 +12,7 @@
 POSTGRES_MAIN_CONTAINER=${POSTGRES_MAIN_CONTAINER:-"postgresdb"}
 PGPORT=${PGPORT:-5432}
 PGUSER=${PGUSER:-"admin"}
-
-PGDATABASE="prefs_testdb"
+PGDATABASE=${PGDATABASE:-"prefs_testdb"}
 
 log() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
--- src/database/scripts/startDockerPostgres.sh
+++ src/database/scripts/startDockerPostgres.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Copyright 2020 OCAD University
+# Copyright 2020-2021 OCAD University
 #
 # Licensed under the New BSD license. You may not use this file except in
-# compliance with this License.
+# compliance with this License.  You may obtain a copy of the License at
+# https://github.com/fluid-project/preferencesServer/blob/main/LICENSE
 
 # Starts up a Postgres database using a docker image.  If it is not present, it
 # is downloaded (pulled) from dockerhub.
@@ -11,7 +12,7 @@
 # Default values
 POSTGRES_MAIN_CONTAINER=${POSTGRES_MAIN_CONTAINER:-"postgresdb"}
 PGPORT=${PGPORT:-5432}
-PGUSER=${PGUSER:-"admin"}
+POSTGRES_USER=${PGUSER:-"admin"}
 PGDATABASE=${PGDATABASE:-"fluid_prefsdb"}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-"asecretpassword"}
 POSTGRES_IMAGE=${POSTGRES_IMAGE:-"postgres:13.1-alpine"}
@@ -20,25 +21,39 @@ log() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
 }
 
+run() {
+docker run -d \
+    --name="$POSTGRES_MAIN_CONTAINER" \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -p $PGPORT:$PGPORT \
+    -d $POSTGRES_IMAGE postgres -p $PGPORT
+}
+
 log "POSTGRES_MAIN_CONTAINER: $POSTGRES_MAIN_CONTAINER"
 log "PGPORT: $PGPORT"
-log "PGUSER: $PGUSER"
+log "POSTGRES_USER: $POSTGRES_USER"
 log "POSTGRES_IMAGE: $POSTGRES_IMAGE"
 log "PGDATABASE: $PGDATABASE"
 
 log "Starting postgres in a docker container, if not already running ..."
-docker run -d \
-    --name="$POSTGRES_MAIN_CONTAINER" \
-    -e PGUSER=$PGUSER \
-    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-    -p $PGPORT:$PGPORT \
-    -d $POSTGRES_IMAGE postgres -p $PGPORT
+
+# Try starting a stopped container, if any.  If that fails, start a new one.
+docker start $POSTGRES_MAIN_CONTAINER
+if [[ $? != 0 ]]; then
+    docker run -d \
+        --name="$POSTGRES_MAIN_CONTAINER" \
+        -e POSTGRES_USER=$POSTGRES_USER \
+        -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+        -p $PGPORT:$PGPORT \
+        -d $POSTGRES_IMAGE postgres -p $PGPORT
+fi
 
 log "Checking that PostgresDB is ready..."
 POSTGRESDB_ISREADY=0
 for i in `seq 1 30`
 do
-    docker exec --user postgres postgresdb pg_isready -p $PGPORT
+    docker exec --user postgres "$POSTGRES_MAIN_CONTAINER" pg_isready -p $PGPORT
     if [[ $? == 0 ]]; then
         POSTGRESDB_ISREADY=1
         break
@@ -49,7 +64,7 @@ done
 if [[ $POSTGRESDB_ISREADY == 1 ]]; then
     log "Creating $PGDATABASE database ..."
     ERR_MSG="$(docker exec $POSTGRES_MAIN_CONTAINER \
-        createdb -p $PGPORT -U $PGUSER --echo $PGDATABASE 2>&1 >/dev/null)"
+        createdb -p $PGPORT -U $POSTGRES_USER --echo $PGDATABASE 2>&1 >/dev/null)"
 
     if [[ $? == 0 ]]; then
         EXIT_STATUS=0
