6da860f9652e32512d84a7ec48925a3617c7b866
Joseph Scheuhammer <clown@alum.mit.edu>
2021-10-18 10:47:28 -0400

    fix: add comment that client id and secret are dummy values for testing

--- src/personalData/test/GoogleSsoProvider.sql
+++ src/personalData/test/GoogleSsoProvider.sql
@@ -9,17 +9,11 @@
 -- You may obtain a copy of the License at
 -- https://github.com/fluid-project/preferencesServer/blob/main/LICENSE
 
+-- Note that this is a dummy client id and secret for testing Google SSO
+
 INSERT INTO "AppSsoProvider" (provider, name, client_id, client_secret) VALUES (
     'google',
     'Google',
     '554291169960-repqllu9q9h5loog0hpadr6854fb2oq0.apps.dummy.com',
     'ek1k4RNTao8XY6gAmmOXxJ6m'
 ) RETURNING *;
-
--- This is a real one.
--- INSERT INTO "AppSsoProvider" (provider, name, client_id, client_secret) VALUES (
---     'google',
---     'google',
---     '247181811838-36gccml0o88mvfeljq3cdchdaja1mj1t.apps.googleusercontent.com',
---     '_Qh6eAUV630XhLLyAayf7z9l'
--- ) RETURNING *;
